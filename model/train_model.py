6c5749e1d338f41b90750b413009cdd998edb528
bhavesh <bhaveshtripathi3112@gmail.com>
2025-05-20 15:35:41 +0530

    updated

--- model/train_model.py
+++ model/train_model.py
@@ -1,4 +1,4 @@
-import pandas as pd
+cd import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
